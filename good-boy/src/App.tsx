4d4589d78c608714a23b26d3acd0726b98542bf0
beredi <43635436+beredi@users.noreply.github.com>
2023-05-03 11:39:04 +0200

    i18n configuration

--- good-boy/src/App.tsx
+++ good-boy/src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
+import { useTranslation } from "react-i18next";
 
 const App = () => {
-  return <div>New App</div>;
+  const { t } = useTranslation();
+  return <div>{t("newApp")}</div>;
 };
 
 export default App;
