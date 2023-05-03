7fe6722f8c12ee76a03036fcb80dca9866903fbb
beredi <43635436+beredi@users.noreply.github.com>
2023-05-03 12:03:00 +0200

    styled-components configuration

--- good-boy/src/App.tsx
+++ good-boy/src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
 import { useTranslation } from "react-i18next";
+import { Container } from "./components/common/UI/Container";
 
 const App = () => {
   const { t } = useTranslation();
-  return <div>{t("newApp")}</div>;
+  return (
+    <Container>
+      <div>{t("newApp")}</div>
+    </Container>
+  );
 };
 
 export default App;
